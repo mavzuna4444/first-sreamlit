2526291fec6d178831d30d251d253c65f264c00b
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-10-07 11:35:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,22 @@ with st.expander('alzheimers_disease_patient_data.csv'):
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
   st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
+
+import streamlit as st
+import pandas as pd
+
+with st.expander('Initial data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  
+  st.write('**X**')
+  X_raw = df.drop('species', axis=1)
+  X_raw
+
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
+
+with st.expander('Data visualization'):
+  st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
+  st.scatter_chart(data=df, x='bill_depth_mm', y='sex', color='species')
+
