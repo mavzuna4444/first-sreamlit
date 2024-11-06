47556e1920968b291d29b4e174ee098c74ccbbc6
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-11-06 21:50:10 +0500

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.title('😁 Abubakr First APP')
 st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
-  df = pd.read_csv('https://www.kaggle.com/datasets/sulimanabusamak123/alzheimers-public-dataset')  
+  df = pd.read_csv('https://github.com/mavzuna4444/first-sreamlit/blob/master/alzheimers_disease_patient_data.csv')  
   st.write('**X**')
   X_raw = df.drop('species', axis=1)
   X_raw
