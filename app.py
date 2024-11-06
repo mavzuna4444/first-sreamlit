84453eb86ef90bc4e74e20bc6124c0e7c76d6501
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-11-06 23:45:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,4 @@ st.info('This is app builds a machine learning model!')
 
 with st.expander('Data'):
   df = pd.read_csv('alzheimers_disease_patient_data.csv')  
-  st.write('**X**')
-  X_raw = df.drop('species', axis=1)
-  X_raw
-
-  st.write('**y**')
-  y_raw = df.species
-  y_raw
+  df
