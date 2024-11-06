13750a9e3a5bd62ad6efd71cc3a5c8fca596a2d8
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-11-07 02:10:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,5 @@ with st.sidebar:
          'Difficulty Completing Tasks': [Difficulty_Completing_Tasks],
          'Forgetfulness':	[Forgetfulness]})
 
-
+ if st.button("Предсказать"):
+    st.subheader(f"Предсказанное состояние ментального здоровья:")
