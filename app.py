38c4d53444ee02331a08da443498bc0094a858c7
mavzuna4444 <162799817+mavzuna4444@users.noreply.github.com>
2024-11-06 23:49:07 +0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,11 @@ st.info('This is app builds a machine learning model!')
 with st.expander('Data'):
   df = pd.read_csv('alzheimers_disease_patient_data.csv')  
   df
-
-info = df.info()
-info
+with st.sidebar:
+  st.header('Input features')
+  island = st.selectbox('Island', ('Biscoe', 'Dream', 'Torgersen'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 43.9)
+  bill_depth_mm = st.slider('Bill depth (mm)', 13.1, 21.5, 17.2)
+  flipper_length_mm = st.slider('Flipper length (mm)', 172.0, 231.0, 201.0)
+  body_mass_g = st.slider('Body mass (g)', 2700.0, 6300.0, 4207.0)
+  gender = st.selectbox('Gender', ('male', 'female'))
